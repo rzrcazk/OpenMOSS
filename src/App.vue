8015353aba0f6e5ed1aeff063fe66ee868244c9c
XiaoHuang <52094985+uluckyXH@users.noreply.github.com>
2026-04-01 20:33:16 +0800

    feat: initialize webui orphan branch with frontend source code

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import { Toaster } from '@/components/ui/sonner'
+</script>
+
+<template>
+  <Toaster position="top-center" :duration="2000" rich-colors />
+  <RouterView />
+</template>
